5350d793754c468f28ffda1e916cbd6ebe9ac0e2
Anthony <antabrrue@alu.edu.gva.es>
2024-09-12 18:04:24 +0200

    1 eval y holamundo php

---
+++ 1Evaluacion/holamundo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1>Este es mi primer programa local</h1>
+    <?php echo "Hola Mundo"; ?>
+</body>
+</html>
