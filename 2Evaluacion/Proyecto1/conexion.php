47dfce3a911abbebd590c8e319dfcd73f5553ee0
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-12-05 18:21:39 +0100

    arreglada conexion con phpmyadmin 1234

--- 2Evaluacion/Proyecto1/conexion.php
+++ 2Evaluacion/Proyecto1/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 function conexion() {
-    $mysqli_conexion = new mysqli("127.0.0.1", "usuarioBD", "12345678", "tienda");
+    $mysqli_conexion = new mysqli("127.0.0.1", "phpmyadmin", "1234", "tienda");
     
     if ($mysqli_conexion->connect_errno) {
         echo "Error de conexión: " . $mysqli_conexion->connect_errno;
