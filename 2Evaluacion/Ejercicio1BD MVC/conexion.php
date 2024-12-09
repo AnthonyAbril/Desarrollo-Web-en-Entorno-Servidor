c20ab2c0d1e87c41fe8efd925e5301d240e9fc91
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-12-09 16:45:50 +0100

    ordenado

---
+++ 2Evaluacion/Ejercicio1BD MVC/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+function conexion() {
+    $mysqli_conexion = new mysqli("127.0.0.1", "phpmyadmin", "1234", "tienda");
+    
+    if ($mysqli_conexion->connect_errno) {
+        echo "Error de conexión: " . $mysqli_conexion->connect_errno;
+        exit;
+    }
+    
+    return $mysqli_conexion;
+}
+
+?>
