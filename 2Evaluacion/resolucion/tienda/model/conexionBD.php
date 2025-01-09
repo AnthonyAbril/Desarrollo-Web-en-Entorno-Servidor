21fb8a079ba8507177885af36e53f1f9689463b1
AnthonyAbril <antabrrue@alu.edu.gva.es>
2025-01-09 18:57:30 +0100

    php resolucion conexion bbdd con MVC

--- 2Evaluacion/resolucion/tienda/model/conexionBD.php
+++ 2Evaluacion/resolucion/tienda/model/conexionBD.php
@@ -2,27 +2,29 @@
 
 class conexionBD {
 
+    //datos para la conexion con la BBDD
     private static $hostname = "127.0.0.1";
     private static $database = "tienda";
-    private static $user = "root";
+    private static $user = "phpmyadmin";
     private static $password = "1234";
 
     public static function conectar(){
         try {
+            //Hace una conexion a la base de datos
             $conexion = new mysqli(self::$hostname, self::$user, self::$password, self::$database);    
             
         } catch (mysqli_sql_exception $error) {
-            
+            //mensaje de error si no le permite
             echo "¡ERROR: !".$error->getMessage();
-            die();
+            die();//finaliza la funcion
             
         }
         
-        return $conexion;
+        return $conexion;//devuelve la conexion realizada
     }
 
     public static function cerrarConexion($conexion){
-        $conexion->close();
+        $conexion->close();//cierra la conexion realizada
     }
 }
 
--- 2Evaluacion/resolucion/tienda/views/showVentasProductos.php
+++ 2Evaluacion/resolucion/tienda/views/showVentasProductos.php
@@ -10,12 +10,20 @@
 <body>
     <h1>Lista de Ventas por Producto</h1>
     <form action="" method="post" id="form_select_product">
+
         <label for="producto"></label>
+
+        <!-- SELECTOR DE PRODUCTOS -->
         <select name="producto" id="producto" onchange="submitOnChange('form_select_product')">
-            <option value="">Seleccione un producto</option>
+
+            <option value="">Seleccione un producto</option><!-- opcion por defecto -->
             <?php
+            // Recorre los productos
             foreach($productos as $producto){
+                //si el producto ha sido seleccionado en el POST (al enviar la peticion) lo guarda para dejarlo seleccionado
                 $selected = ($producto['id_producto']==$productoSeleccionado)?"selected":"";
+
+                //Crea una opcion por cada producto
                 echo '<option value="' . $producto['id_producto'] . '" '. $selected . '>' . $producto['nombre'] . '</option>';
             }
             ?>
@@ -23,7 +31,9 @@
     </form>
 
     <?php
+    //si hay alguna vevnta del producto seleccionado (y si hay producto seleccionado)
     if (count($ventasProducto)>0) {
+        //se crea una tabla con la lista de sus ventas
     ?>
         <table>
             <tr>
